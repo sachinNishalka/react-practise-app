f019a4c7084d4528c1c34c4c413e63d4ce42dcd1
sachinNishalka <nishalkasachin@gmail.com>
2022-05-31 08:40:42 -0700

    react-props-tutorial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import Courses from "./components/Courses"
+import "./App.css"
 
 export default function App() {
   return (
     <div>
-      <div>App</div>
-      <div>This code is written in first-change branch</div>
+      <h1>Course List</h1>
+      <hr />
+      <div className='cards'>
+      <Courses name="sachin" course="c#"></Courses>
+      <Courses name="nishalka" course="python"></Courses>
+      <Courses name="fernando" course="javascript"></Courses>
+      <Courses name="james" course="php"></Courses>
+      </div>
     </div>
   )
 }
